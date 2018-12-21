5b09e9c73576400008337108be4d99d1b05bd722
Anthony Zukaitis <azukaitis@gmail.com>
2018-12-21 14:34:31 -0700

    Added option to specify delimiter of namespace

--- json_to_cmake.py
+++ json_to_cmake.py
@@ -4,36 +4,37 @@ import json
 import pprint
 import argparse
 
-def flatten_json(y,namespace="json"):
-        name=namespace+"."
+def flatten_json(y,namespace="json",delim="."):
+        name=namespace+delim
 	out={}
-	def flatten(x, name=''):
+	def flatten(x, name='',delim=delim):
 		if type(x) is dict:
 			for a in x:
-				flatten(x[a], name + a + '.')
-			out[name[:-1]]=list(x.keys())
+				flatten(x[a], name + a + delim)
+			out[name[:-len(delim)]]=list(x.keys())
 
 		elif type(x) is list:
 			i=0
 			for a in x:
-				flatten(a, name + str(i) + '.')
+				flatten(a, name + str(i) + delim)
 				i+=1
-			out[name[:-1]]=list(range(0,i))
+			out[name[:-len(delim)]]=list(range(0,i))
 		else:
-			out[name[:-1]] = x
+			out[name[:-len(delim)]] = x
 
-	flatten(y,name=namespace+".")
+	flatten(y,name=namespace+delim)
 	return out
 
 parser = argparse.ArgumentParser(description="Flatten a json file into single name value pairs for use with CMake.")
 parser.add_argument("jsonfile", help="Name of json file to flatten",type=str)
 parser.add_argument("--output", help="Name of output file to write to",type=str)
 parser.add_argument("--name", help="Namespace to use for json variables",type=str,default="json")
+parser.add_argument("--delim", help="Delimiter to use for namespace separation",type=str,default=".")
 args=parser.parse_args()
 
 with open(args.jsonfile,'r') as json_file:
 	json_obj = json.load(json_file)
-        flat=flatten_json(json_obj,namespace=args.name)
+        flat=flatten_json(json_obj,namespace=args.name,delim=args.delim)
 	if( args.output ) : 
 		f1=open(args.output,"w")
 	for key in flat:
